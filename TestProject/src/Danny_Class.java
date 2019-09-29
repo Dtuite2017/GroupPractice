ee391b4abff77d6e285fd2d7c7e172cf43f33828
danny <danny.a.tuite@gmail.com>
2019-09-29 14:48:30 +0100

    New file added within a branch connected to Development

---
+++ TestProject/src/Danny_Class.java
@@ -0,0 +1,37 @@
+public class Danny_Class {
+ private string name;
+ private int age;
+
+    public Danny_Class(string name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Danny_Class() {
+    }
+
+    public string getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Danny_Class{" +
+                "name=" + name +
+                ", age=" + age +
+                '}';
+    }
+    
+}
