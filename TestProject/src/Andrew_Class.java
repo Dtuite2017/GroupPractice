5a2b9552b775b0965f59d7eaa037d194137b0a63
Andrew Tavey <d00195871@student.dkit.ie>
2019-10-18 13:55:59 +0100

    Andrew test

---
+++ TestProject/src/Andrew_Class.java
@@ -0,0 +1,37 @@
+public class Andrew_Class {
+    private string name;
+    private int age;
+
+    public Andrew_Class(string name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Andrew_Class() {
+    }
+
+    public string getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(string name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Andrew_Class{" +
+                "name=" + name +
+                ", age=" + age +
+                '}';
+    }
+
+}
--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Test application for Learning GIT");
-
+        System.out.println("Hi Andrew");
     }
 }
