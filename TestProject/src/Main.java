77b9b14d08696c3d6fddf83e497d3dc7fc76057d
Carol Mullen <d00196117@student.dkit.ie>
2019-10-18 13:55:56 +0100

    test commit

--- TestProject/src/Main.java
+++ TestProject/src/Main.java
@@ -2,5 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Test application for Learning GIT");
 
+        System.out.println("Carol");
+
     }
 }
